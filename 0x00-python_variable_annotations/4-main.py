e9595f6f1dd8e1b9c7ff77099a90f5249bc830ea
kinyarasam <skinyara.30@gmail.com>
2023-01-05 21:50:50 +0300

    7. Complex types - string and int/float to tuple

--- 0x00-python_variable_annotations/4-main.py
+++ 0x00-python_variable_annotations/4-main.py
@@ -2,7 +2,8 @@
 
 a = __import__('4-define_variables').a
 pi = __import__('4-define_variables').pi
-i_understand_annotations = __import__('4-define_variables').i_understand_annotations
+i_understand_annotations = __import__(
+        '4-define_variables').i_understand_annotations
 school = __import__('4-define_variables').school
 
 print("a is a {} with a value of {}".format(type(a), a))
