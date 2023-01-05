2a7986175e0160edd6f8f6f1ea3e6efa22d395aa
kinyarasam <skinyara.30@gmail.com>
2023-01-05 21:01:29 +0300

    Add type annotations to variables" (if the commit adds type annotations to the variables)

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-a: int = 1 # An integer with a value of 1
-pi: float = 3.14 # A float with a value of 3.14
+# An integer with a value of 1
+a: int = 1
+
+# A float with a value of 3.14
+pi: float = 3.14
 
 # A boolean with a value of True
 i_understand_annotations: bool = True
